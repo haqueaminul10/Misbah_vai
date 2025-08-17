3da27eae2cd1bff8e5c6dccda5fe8b4835fb30a1
haqueaminul10 <haqueaminul10100@gmail.com>
2025-08-17 19:12:00 +0600

    packege responsive done

--- src/components/packages/carousel.tsx
+++ src/components/packages/carousel.tsx
@@ -69,31 +69,41 @@ const CarouselItems = () => {
         <CarouselContent>
           {packages.length > 0 &&
             packages.map((data) => (
-              <CarouselItem key={data.id} className='basis-[27%] shrink-0'>
+              <CarouselItem
+                key={data.id}
+                className='
+                  basis-[90%] xs:basis-[70%] sm:basis-[48%] md:basis-[32%] lg:basis-[27%]
+                  shrink-0 px-1
+                '
+              >
                 <Card
-                  className='group h-64 bg-cover bg-center rounded-xl shadow-lg hover:border-2 hover:border-orange-700'
+                  className='
+                    group h-48 xs:h-56 sm:h-60 md:h-64
+                    bg-cover bg-center rounded-xl shadow-lg
+                    hover:border-2 hover:border-orange-700
+                  '
                   style={{ backgroundImage: `url(${data.imageUrl.src})` }}
                 >
                   <CardContent className='h-full'>
                     <div className='h-full flex items-end justify-between'>
-                      <div className='text-white font-medium text-2xl w-64'>
+                      <div className='text-white font-medium text-base xs:text-lg sm:text-xl md:text-2xl w-40 xs:w-48 sm:w-56 md:w-64'>
                         {data.title}
                       </div>
                       <div className='border-2 border-orange-700 p-1 rounded-md text-white opacity-0 group-hover:opacity-100 transition-opacity duration-200'>
                         <MoveRight />
                       </div>
                     </div>
-                    <div className='text-white flex items-center text-xs'>
+                    <div className='text-white flex items-center text-xs sm:text-sm mt-1'>
                       <Tag className='h-4' />
-                      <span>{data.startingPrice}</span>
+                      <span className='ml-1'>{data.startingPrice}</span>
                     </div>
                   </CardContent>
                 </Card>
               </CarouselItem>
             ))}
         </CarouselContent>
-        <CarouselPrevious className='absolute left-2 top-1/2 transform -translate-y-1/2 w-10 h-10 bg-white rounded-full shadow-md text-black hover:bg-gray-200' />
-        <CarouselNext className='absolute right-2 top-1/2 transform -translate-y-1/2 w-10 h-10 bg-white rounded-full shadow-md text-black hover:bg-gray-200' />
+        <CarouselPrevious className='absolute left-1 xs:left-2 top-1/2 transform -translate-y-1/2 w-8 h-8 xs:w-10 xs:h-10 bg-white rounded-full shadow-md text-black hover:bg-gray-200' />
+        <CarouselNext className='absolute right-1 xs:right-2 top-1/2 transform -translate-y-1/2 w-8 h-8 xs:w-10 xs:h-10 bg-white rounded-full shadow-md text-black hover:bg-gray-200' />
       </Carousel>
     </div>
   );
